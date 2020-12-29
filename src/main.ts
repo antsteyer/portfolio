c448ce6f6fd6566f7d1093fb4002b9abd0f2d5c2
antsteyer <antoine.steyer@hey.com>
2020-12-29 10:47:43 +0100

    Revert tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import "./index.css";
 
 Vue.config.productionTip = false;
 
---
+++ src/views/Contact.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>Le début d'une longe relation !</h1>
+
+    <a href="">Twitter</a>
+    <a href="">LinkedIn</a>
+  </div>
+</template>
